912748acfa5a8969df8831b17023f5bd6b7ae10a
rinonkia <gatapon1988@gmail.com>
2024-06-27 02:51:06 +0900

    Update go version 1.21.0 for gin

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(
+			http.StatusOK,
+			gin.H{},
+		)
+	})
+	if err := r.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
